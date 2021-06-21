19e26fc6ca1a3c1047104e5bbe080d9adf4989d1
“Your <shakil3300bd@gmail.com>
2021-06-21 13:18:13 +0600

    basic of Packages

---
+++ 10. Basic of Package.R
@@ -0,0 +1,14 @@
+#Install a Package
+install.packages("gapminder")
+
+library(gapminder)
+head(gapminder)
+tail(gapminder)
+
+#Check Other Features
+str(gapminder)
+dim(gapminder)
+summary(gapminder)
+
+#See Documentation
+?gapminder
